bfe57c765d00b0a752fa629ed0fd05a11b48a6c2
Reese RJ Kaz <rjkaz@comcast.net>
2021-03-13 23:13:40 -0500

    update

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ let bool = true;
 const sections = document.querySelectorAll("section");
 const progress = document.querySelector('.progress h2');
 const circles = document.querySelectorAll(".circle");
+const menu = document.querySelector(".menu");
+const section1wrapper = document.querySelector('section-1-wrapper')
+const section5wrapper = document.querySelector('section-5-wrapper')
+
+section1wrapper.style.transform = 'scale(1)'
 
 
 const progressCounter = () => {
@@ -24,6 +29,9 @@ const pageController = () => {
         })
         counter1 = 0;
         counter2 = 1;
+        section1wrapper.style.transform = 'scale(1)';
+        section5wrapper.style.transform = 'scale(1.5)';
+        progressCounter();
         bool = false;
     }
 
@@ -36,6 +44,8 @@ const pageController = () => {
         });
         counter1 = 4;
         counter2 = 5;
+        section1wrapper.style.transform = 'scale(1)';
+        section5wrapper.style.transform = 'scale(1.5)';
         progressCounter()
         bool = false;
     }
@@ -53,48 +63,60 @@ window.addEventListener("wheel", (e) => {
         counter1--;
         counter2--;
     }
-/*
-    if (counter1 === 5) {
-        Array.from(sections).forEach(section => {
-            section.style.left = '0'
-        })
-        counter1 = 0;
-        counter2 = 1;
-        return;
-    }
-
-    if (counter1 === -1) {
-        Array.from(sections).forEach(section => {
-            if (section.classList[0] === 'section-5') {
-                return;
-            }
-            section.style.left = "-100vw";
-        });
-        counter1 = 4;
-        counter2 = 5;
-        progressCounter()
-    }
-*/
+    /*
+        if (counter1 === 5) {
+            Array.from(sections).forEach(section => {
+                section.style.left = '0'
+            })
+            counter1 = 0;
+            counter2 = 1;
+            return;
+        }
     
+        if (counter1 === -1) {
+            Array.from(sections).forEach(section => {
+                if (section.classList[0] === 'section-5') {
+                    return;
+                }
+                section.style.left = "-100vw";
+            });
+            counter1 = 4;
+            counter2 = 5;
+            progressCounter()
+        }
+    */
+
     pageController();
     progressCounter();
     console.log(counter1, counter2);
 
-    bool && (document.querySelector(`.section-${deltaY ? counter1 : counter2}`).style.left = `${deltaY ? "-100vw" : "0"}`);
+    if (bool) {
+        document.querySelector(`.section-${deltaY ? counter1 : counter2}`).style.left = `${deltaY ? "-100vw" : "0"}`;
+        document.querySelector(`.section-${deltaY ? counter1 : counter2}-wrapper`).style.transform = `scale(${deltaY ? "1.5" : "1"})`;
+        document.querySelector(`.section-${deltaY ? counter1  + 1 : counter2 + 1}-wrapper`).style.transform = `scale(${deltaY ? "1" : "1.5"})`;
+    }
 
-    
 })
 
 document.querySelector('.left-btn').addEventListener('click', () => {
     counter1--;
     counter2--;
     pageController() && (document.querySelector(`.section-${counter2}`).style.left = "0");
+
+    if(bool) {
+        document.querySelector(`.section-${counter2}-wrapper`).style.transform = 'scale(1)';
+        document.querySelector(`.section-${counter2 + 1}-wrapper`).style.transform = 'scale(1.5)';
+    }
 });
 
 document.querySelector('.right-btn').addEventListener('click', () => {
     counter1++;
     counter2++;
     pageController() && (document.querySelector(`.section-${counter1}`).style.left = "-100vw");
+    if(bool) {
+        document.querySelector(`.section-${counter2}-wrapper`).style.transform = 'scale(1)';
+        document.querySelector(`.section-${counter1}-wrapper`).style.transform = 'scale(1.5)';
+    }
 });
 
 document.querySelector('.grapes-img').addEventListener('mouseover', () => {
